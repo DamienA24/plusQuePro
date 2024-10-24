8ac7c321fa865f6136b49414916e5854547d839d
DamienA <alves.d24@laposte.net>
2024-10-24 21:09:57 +0200

    feat(components): create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-primary text-primary-foreground p-4">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold">TMDB</h1>
+      </div>
+    </header>
+  );
+}
