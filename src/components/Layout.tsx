61ff1b873a762e842a3ec7eabb71876e5e55820b
DamienA <alves.d24@laposte.net>
2024-10-24 21:38:54 +0200

    refactor(layout): change way use layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,15 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
+export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
