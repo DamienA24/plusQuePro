9d0c993ebdc565adb88de4dcfa8a2bd2174b641f
DamienA <alves.d24@laposte.net>
2024-10-24 23:27:38 +0200

    feat(global): use swr to fetch trending movies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+import { FetchOptions } from "@/types";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const fetcher = async <T>(
+  url: string,
+  options: FetchOptions
+): Promise<T> => {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error("An error occurred during the query");
+  }
+  return response.json();
+};
