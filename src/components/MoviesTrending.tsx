e2b6ce64778e759944301afbd151cff68292398d
DamienA <alves.d24@laposte.net>
2024-10-25 20:06:22 +0200

    feat(global): display trending movies list

---
+++ src/components/MoviesTrending.tsx
@@ -0,0 +1,39 @@
+import { useCallback } from "react";
+
+import MovieCard from "@/components/MovieCard";
+import Spinner from "@/components/Spinner";
+
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import { useInfiniteTMDB } from "@/hooks/useInfiniteTMDB";
+
+export default function MoviesTrending() {
+  /* const { data, error, isLoading } = useTMDB<TrendingResponse>(
+    API_ENDPOINTS.trending
+  ); */
+  const { movies, error, isLoading, isLoadingMore, isReachingEnd, loadMore } =
+    useInfiniteTMDB();
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const target = entries[0];
+      if (target.isIntersecting && !isLoadingMore && !isReachingEnd) {
+        loadMore();
+      }
+    },
+    [loadMore, isLoadingMore, isReachingEnd]
+  );
+
+  const [ref] = useIntersectionObserver(handleObserver);
+
+  if (error) return <div>failed to load</div>;
+
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {movies &&
+          movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+      </div>
+      <div ref={ref}>{isLoadingMore && <Spinner />}</div>
+    </>
+  );
+}
--- src/pages/MoviesSearchPage.tsx
+++ src/pages/MoviesSearchPage.tsx
@@ -1,4 +1,11 @@
 import MovieSearchBar from "@/components/MovieSearchBar";
+import MoviesTrending from "@/components/MoviesTrending";
+
 export default function MoviesSearchPage() {
-  return <MovieSearchBar />;
+  return (
+    <>
+      <MovieSearchBar />
+      <MoviesTrending />
+    </>
+  );
 }
